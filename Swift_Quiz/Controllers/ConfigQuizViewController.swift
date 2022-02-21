8e3c067d8f3f80e6be3b48a2cb7b9a06a63c8daf
Diogenes <diogenes.negreiros@consulting-for.edenred.com>
2022-02-20 22:17:24 -0300

    extensão viewController e coreData

--- Swift_Quiz/Controllers/ConfigQuizViewController.swift
+++ Swift_Quiz/Controllers/ConfigQuizViewController.swift
@@ -6,26 +6,27 @@
 //
 
 import UIKit
+import CoreData
 
 class ConfigQuizViewController: UIViewController {
-    @IBOutlet weak var question: UITextView!
     
+    @IBOutlet weak var question: UITextView!
     @IBOutlet weak var answer1: UITextField!
     @IBOutlet weak var answer2: UITextField!
-    
     @IBOutlet weak var answer3: UITextField!
-    
     @IBOutlet weak var correctAnswer: UITextField!
     @IBOutlet weak var answer4: UITextField!
     
+    var questao: Questao!
+    
     @IBAction func addButtonAction(_ sender: Any) {
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        questao.acerto = false
     }
-
 }
 
 extension ConfigQuizViewController {
---
+++ Swift_Quiz/Extensions/ViewController+CoreData.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController+CoreData.swift
+//  Swift_Quiz
+//
+//  Created by Diogenes de Souza Negreiros on 20/02/22.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+extension UIViewController {
+    
+    var context: NSManagedObjectContext {
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
+    }
+    
+}
