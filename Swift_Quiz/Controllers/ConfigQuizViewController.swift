2128a7dfbd0fb3a050f6b8868c57d81d33bb33ae
Diogenes <diogenes.negreiros@consulting-for.edenred.com>
2022-02-20 22:40:34 -0300

    action add

--- Swift_Quiz/Controllers/ConfigQuizViewController.swift
+++ Swift_Quiz/Controllers/ConfigQuizViewController.swift
@@ -14,18 +14,23 @@ class ConfigQuizViewController: UIViewController {
     @IBOutlet weak var answer1: UITextField!
     @IBOutlet weak var answer2: UITextField!
     @IBOutlet weak var answer3: UITextField!
-    @IBOutlet weak var correctAnswer: UITextField!
     @IBOutlet weak var answer4: UITextField!
+    @IBOutlet weak var correctAnswer: UITextField!
     
-    var questao: Questao!
+    var myQuestion: Question!
     
     @IBAction func addButtonAction(_ sender: Any) {
-        
+        myQuestion = Question(context: context)
+        myQuestion.statement = question.text
+        myQuestion.alternativa?.texto1 = answer1.text
+        myQuestion.alternativa?.texto2 = answer2.text
+        myQuestion.alternativa?.texto3 = answer3.text
+        myQuestion.alternativa?.texto4 = answer4.text
+        myQuestion.answer = correctAnswer.text
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        questao.acerto = false
     }
 }
 
