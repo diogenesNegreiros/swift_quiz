632539398f0489496fbc2f93ca8bc374f5690eae
Diogenes <diogenes.negreiros@consulting-for.edenred.com>
2022-02-23 23:36:46 -0300

    esta dobrando as perguntas

--- Swift_Quiz/Model/QuizManager.swift
+++ Swift_Quiz/Model/QuizManager.swift
@@ -10,58 +10,13 @@ import UIKit
 import CoreData
 
 class QuizManager {
+    private var quizes: [Question] = []
     
-//    var quest = Question()
-    
-    var quizes: [Question] = []
-
-    
-//    private var quizes: [(question: String, correctAnswer: String, options: [String])] = [
-//        (question: "Quais desses é string?", correctAnswer: "\"Olá mundo\"",
-//         options: ["20", "\"Olá mundo\"", "Olá mundo", "'Olá mundo'"]),
-//        (question: "Qual é o resultado da expressão \"1\" + \"1\"", correctAnswer: "\"11\"",
-//         options: ["11", "\"2\"", "\"11\"", "\"1 1\""]),
-//        (question: "Qual desses símbolos é usado para \"escapar\" caracteres especiais", correctAnswer: "\\",
-//         options: ["/", "\\", "!", "ESC"]),
-//        (question: "Qual a sintaxe correta para declarar uma função?", correctAnswer: "func myFunction() {}",
-//         options: ["func = myFunction() {}", "let myFunction = func() {}", "let func = myFunction() {}", "func myFunction() {}"]),
-//        (question:
-//            """
-//                 Qual a saída desse código?
-//                   func printSomething() {
-//                      print("Adoro funções")
-//                   }
-//                   printSomething()
-//                """, correctAnswer: "Adoro funções",
-//         options: ["Nada", "printSomething()", "Adoro funções", "Erro"]),
-//        (question: "Quando não declaramos o tipo de retorno de uma função, qual é o retorno padrão?", correctAnswer: "Void",
-//         options: ["Void", "Int", "String", "nil"]),
-//        (question: "Qual é a sintaxe correta para declarar um parâmetro?", correctAnswer: "func myFunc(message: String) {}",
-//         options: ["func myFunc(String message) {}", "func myFunc(let message = String) {}", "func myFunc(message String) {}", "func myFunc(message: String) {}"]),
-//
-//        (question: "Qual é o tipo de uma variavél String que pode aceitar nil?", correctAnswer: "String?",
-//         options: ["String", "String?", "Int", "Void"]),
-//        (question: "Quando queremos declarar uma constante, qual palavra reservada devemos usar?", correctAnswer: "let",
-//         options: ["var", "const", "let", "CONST"]),
-//        (question: "O que é um dicionário?", correctAnswer: "É uma coleção não-ordenada com chave e valor",
-//         options: ["É uma coleção que não aceita objetos repetidos", "É uma coleção ordenada com chave e valor", "É uma coleção não-ordenada com chave e valor", "É uma coleção que permite valores de tipos diferentes"]),
-//        (question: "Qual o nome do tipo especial em Swift que é usado para declarar blocos ou funções anônimas?", correctAnswer: "Closure",
-//         options: ["Lambda", "Block", "Closure", "Protocol"]),
-//        (question: "O que é um enum?", correctAnswer: "Tipo definido pelo usuário para um grupo de valores relacionados",
-//         options: ["É um conjunto de Strings", "É um tipo especial de String", "É um operador unário do tipo prefix", "Tipo definido pelo usuário para um grupo de valores relacionados"]),
-//        (question: "O que uma extension não pode fazer?", correctAnswer: "Adicionar propriedades armazenadas",
-//         options: ["Adicionar novas funcionalidades", "Definir novos construtores", "Adicionar propriedades armazenadas", "Adicionar propriedades computadas"]),
-//        (question: "Quando herdamos de uma classe e queremos chamar o seu construtor, usamos...", correctAnswer: "super.init",
-//         options: ["main.init", "super.init", "self.init", "super"]),
-//    ]
-    
-    var context: NSManagedObjectContext {
-    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    private var context: NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
         return appDelegate.persistentContainer.viewContext
     }
-    
-    var fetchedResultController: NSFetchedResultsController<Question>!
-    
+    private var fetchedResultController: NSFetchedResultsController<Question>!
     private var quiz = Question()
     private var _totalAnswers = 0
     private var _totalCorrectAnswers = 0
@@ -87,18 +42,17 @@ class QuizManager {
     }
     
     init() {
-        print("")
+//        quiz = Question(context: self.context)
+        //        quiz.statement = "Qual o nome desse App de Perguntas e respostas?"
+        //        quiz.answer1 = "Quiz Manager"
+        //        quiz.answer2 = "Quiz Brazuca"
+        //        quiz.answer3 = "Study Quiz"
+        //        quiz.answer4 = "Swift Quiz"
+        //        quiz.answer = "Swift Quiz"
+        //
+        //        self.quizes.append(self.quiz)
         
-        quiz = Question(context: self.context)
-        
-        quiz.statement = "Qual o nome desse App de Perguntas e respostas?"
-        quiz.answer1 = "Quiz Manager"
-        quiz.answer2 = "Quiz Brazuca"
-        quiz.answer3 = "Study Quiz"
-        quiz.answer4 = "Swift Quiz"
-        quiz.answer = "Swift Quiz"
-
-        self.quizes.append(self.quiz)
+        loadAllQuizes()
     }
     
     func addNewQuiz(quiz: Question) {
@@ -122,14 +76,27 @@ class QuizManager {
     }
     
     func loadAllQuizes() {
+        quizes = []
         let fetchRequest: NSFetchRequest<Question> = Question.fetchRequest()
         let sortDescritor = NSSortDescriptor(key: "statement", ascending: true)
         fetchRequest.sortDescriptors  = [sortDescritor]
         fetchedResultController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
-//        fetchedResultController.delegate = self
         do{
-        try fetchedResultController.performFetch()
-            self.quizes = fetchedResultController.fetchedObjects ?? []
+            try fetchedResultController.performFetch()
+            
+            if let myQuizes = fetchedResultController.fetchedObjects {
+                quizes = myQuizes
+            }else {
+                quiz = Question(context: self.context)
+                quiz.statement = "Qual o nome desse App de Perguntas e respostas?"
+                quiz.answer1 = "Quiz Manager"
+                quiz.answer2 = "Quiz Brazuca"
+                quiz.answer3 = "Study Quiz"
+                quiz.answer4 = "Swift Quiz"
+                quiz.answer = "Swift Quiz"
+                
+                self.quizes.append(self.quiz)
+            }
         }catch {
             print(error.localizedDescription)
         }
