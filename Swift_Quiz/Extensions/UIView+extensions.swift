288651d501802ccdc5680383e338d72e123c4a72
Diogenes <diogenes.negreiros@consulting-for.edenred.com>
2022-02-25 17:23:40 -0300

    teste

---
+++ Swift_Quiz/Extensions/UIView+extensions.swift
@@ -0,0 +1,50 @@
+//
+//  UIView+extensions.swift
+//  Swift_Quiz
+//
+//  Created by Diogenes de Souza Negreiros on 25/02/22.
+//
+
+import Foundation
+import UIKit
+
+
+extension UIView {
+    
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.borderColor = color.cgColor
+            } else {
+                layer.borderColor = nil
+            }
+        }
+    }
+}
